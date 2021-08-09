4b7feb254cc360584310850a8a5960cc7633dd37
Abdul Basit <abdul.basit@7vals.com>
2021-08-09 16:50:34 +0500

    Add Event completed and tested

--- calendar.rb
+++ calendar.rb
@@ -6,32 +6,22 @@ class Calendar
     @events = Hash.new { |h, k| h[k] = [] }
   end 
 
-  def AddEvent
-    puts "Please enter the title: "
-    title = gets
-    puts "Please enter the venue: "
-    venue = gets 
-    puts "Please enter the date(dd/mm/yyyy): "
-    date = Validations.InputDate
+  def AddEvent(date, time, venue, title)
     if date.nil?
       puts "Error in date. Exiting back to main menu..."
       return 
     end 
-    puts "Please enter the time(HH:MM): "
-    time = Validations.InputTime 
     if time.nil?
       puts "Error in time. Exiting back to main menu..."
       return
     end 
-    event_to_add = Event.new(date, time, venue, title)
-    @events[date.month].push(event_to_add)
-
+    event_to_add = Event.new(date, time, title, venue)
+    @events[Date.parse(date).month].push(event_to_add)
     p @events
     return "success"
-
-
   end 
 
+  
 
 
 end
--- main.rb
+++ main.rb
@@ -1,11 +1,37 @@
 require_relative("calendar")
-
+require_relative("validations")
 class Driver 
+  include Validations
   def initialize
     @calendar = Calendar.new
   end
   def run
-    @calendar.AddEvent
+    puts "Welcome ! Today is #{Time.new.day}/#{Time.new.month}/#{Time.new.year} "
+    self.MainMenu
+
+
+  end
+
+  def AddEvent
+    puts "Please enter the title: "
+    title = gets
+    puts "Please enter the venue: "
+    venue = gets 
+    puts "Please enter the date(dd/mm/yyyy): "
+    date = Validations.InputDate
+    puts "Please enter the time(HH:MM): "
+    time = Validations.InputTime
+    @calendar.AddEvent(date, time, title, venue)
+  end
+
+  def MainMenu
+    puts "1-- View the whole month\n"
+    puts "2-- Add Event\n"
+    puts "3-- Remove Event\n"
+    puts "4-- Update Event\n"
+    puts "5-- Show events of a specific month\n"
+    puts "6-- Show events of a specific day\n"
+    puts "7-- Load From A File\n"
   end
 
 
